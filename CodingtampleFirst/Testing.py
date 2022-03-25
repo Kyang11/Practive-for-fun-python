7041f7335c0730386a9bea314abc20d17ae3fcd3
Ku Yang <kum.yaj@gmail.com>
2022-03-25 16:08:26 -0500

    first commit

---
+++ CodingtampleFirst/Testing.py
@@ -0,0 +1,139 @@
+
+"""names = ['pheket', 'Laos', 'Tokyo', 'China', 'Newyork']
+
+for name in names:
+    print(name.title() +' is a great place to visit afte covid')
+    
+    print('I can not wait until covid over so i can go visit'+ name.title() +".\n")
+print("Thank you for the all people\n")
+"""
+
+"""squres=[]
+for value in range(1,11):
+ 
+    squres.append(value**2)
+
+    
+print(squres)"""
+
+
+"""squre =[value**2 for value in range(1,11)]
+print(squre)"""
+
+"""player = ['pheket', 'Laos', 'Tokyo', 'China', 'Newyork']
+
+for players in player[3:]:
+    print(players.title())"""
+    
+
+"""my_list = ["pikachu","mew", "mewtwo", "chrizard", "koj", "kuv"]
+
+my_string = ""
+for index, object in enumerate(my_list):
+    my_string += object + " "
+    if index ==3 or index ==4:
+        my_string += "and "
+print(my_string)"""
+
+
+"""my_list = ["chicken wing","chicken wing", "hot dog", "bologna", "chicken", "macaroni"]
+for food in my_list:
+    if food == "chick wing":
+       break
+    print(food, end =" ")"""
+    
+    
+"""cars =["audi", "bmw", "subaru", "toyota"]
+
+for car in cars:
+    if car == '':
+        print(car.upper())
+    else:
+        print(car.title())"""
+        
+        
+"""ban= ["andrew", "carolina", "david"]
+user="andrew"
+if user not in ban:
+    print(user.title() + ", you re right ")
+else:
+    print(" within the range ")"""
+
+
+"""cars = input("Enter the car now: ")
+
+if cars == "subaru":
+    print("The prediction is true which is " + cars.title())
+elif cars =="audi":
+        print(" My preditcion is false, the car is " + cars.title())
+
+else:
+        print("something else")"""
+        
+
+""""request_stopped = ["mushrooms", "extra cheese"]
+if "mushrooms" in request_stopped:
+    print("adding mushroom to it")
+if "pepperoni" in request_stopped:
+    print("adding pepperoni")
+if "extra cheese" in request_stopped:
+    print("adding extra cheese")
+    
+print("\n Finish ordering")"""
+
+"""request_stopped = ["mushrooms", "green peppers", "extra cheese"]
+for request in request_stopped:
+ print("adding " + request+ ".")
+print("\n finish making pizza")"""
+
+"""request_stopped = ["mushrooms", "green peppers", "extra cheese"]
+for request in request_stopped:
+    if request =="green peppers":
+        print("sorry we are out of green peppers")
+    else:
+        print(" adding "+ request+"." )
+print("\n  end the order")"""
+
+"""x=5
+print(" A spoon", end=" ")
+if x==4 or x==5:
+    print("full of", end=" ")
+elif x>=4: 
+    print("sugar", end=" ")
+else:
+    print("help the medicine", end=" ")
+print("go down", end = " ")"""
+
+
+alien = {
+    'color': 'Green',
+    'point': 5}
+print(alien['color'])
+print(alien['point'])
+    
+
+    
+        
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+        
